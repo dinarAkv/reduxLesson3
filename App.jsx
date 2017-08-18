4d7efb625c041469b5522dad9adf6f7e70de2ada
ITECO\akhmetzyanov <akhmetzyanov@i-teco.ru>
2017-08-18 14:15:22 +0300

    Work example.

---
+++ App.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+
+class App extends Component{
+
+
+
+
+
+  render(){
+    return(
+      <div>
+        <input type="text" />
+        <button>Add track</button>
+        <ul>
+          {this.props.testStore.map((track, index) =>
+            <li key={index}>{track}</li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+
+
+
+}
+
+
+
+
+
+export default connect(
+  state => ({
+    testStore: state
+  }),
+  dispatch => ({})
+)(App);
---
+++ main.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+
+
+const initialState = [
+  'Smells like spirit',
+  'Enter Sandman'
+];
+
+
+function playlist(state = [], action) {
+  if (action.type === 'ADD_TRACK') {
+    return [
+      ...state,
+      action.payload
+    ];
+  }
+  return state;
+}
+
+
+const store = createStore(playlist);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app'));
+
+
+
+
+
+
+//
+//
+//
+// const addTrackBtn = document.querySelectorAll('.addTrack')[0];
+// const trackInput = document.querySelectorAll('.trackInput')[0];
+// const list = document.querySelectorAll('.list')[0];
+//
+// store.subscribe(() => {
+//   list.innerHTML = '';
+//   trackInput.value = '';
+//   store.getState().forEach(track =>{
+//     const li = document.createElement('li');
+//     li.textContent = track;
+//     list.appendChild(li);
+//   })
+//
+// })
+//
+//
+//
+// addTrackBtn.addEventListener('click', () => {
+//   const trackName = trackInput.value;
+//   store.dispatch({ type: 'ADD_TRACK', payload: trackName})
+// })
